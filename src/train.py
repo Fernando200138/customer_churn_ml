54aa1cec0e25cd2978a81c6c2dcf088d0e8f86cc
Fernando Flores Mendoza <f.flores.mendoza@rug.nl>
2026-01-31 12:45:49 -0600

    Applied Grid Search to improve recall metric. Updated Readme file.

--- src/train.py
+++ src/train.py
@@ -6,13 +6,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import src.data as data
-import sklearn.metrics as metrics
-from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, precision_recall_curve, average_precision_score
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, precision_recall_curve, average_precision_score, recall_score
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.linear_model import LogisticRegression
+from scipy.stats import uniform, loguniform
 
 
 def basic_logistic_regression(path_file, plot_precison_recall=False):
@@ -63,7 +64,104 @@ This that we are catching must churners even though we are less accurate at clas
 no-churners.
 In conclusion, we will try to optimize for recall
 '''
+def model_comparison(path_file):
+    df = data.load_data(path_file)
+    df = data.clean_basic_issues(df)
+    # In this model we will do feature selection manually
+    df_encoded = data.deeper_clean(df, OneHotEncoding=True)
+    # Now we can split the data into training data and test data
+    X = df_encoded.drop("Churn_Yes", axis=1)
+    y = df_encoded["Churn_Yes"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    xgb_pipe = make_pipeline(StandardScaler(),XGBClassifier(n_estimators=100))
+    xgb_pipe.fit(X_train, y_train)
+    y_pred_xgb = xgb_pipe.predict(X_test)
+    recall_xgb = recall_score(y_test, y_pred_xgb)
+
+    rfc_pipe = make_pipeline(StandardScaler(),RandomForestClassifier(n_estimators=100))
+    rfc_pipe.fit(X_train, y_train)
+    y_pred_rfc = rfc_pipe.predict(X_test)
+    recall_rfc = recall_score(y_test, y_pred_rfc)
+
+    logistic_pipe = make_pipeline(StandardScaler(),LogisticRegression(class_weight='balanced'))
+    logistic_pipe.fit(X_train, y_train)
+    y_pred_logistic = logistic_pipe.predict(X_test)
+    recall_logistic = recall_score(y_test, y_pred_logistic)
+    print(f'XGBoost Recall: {recall_xgb}')
+    print(f'Random Forest Recall: {recall_rfc}')
+    print(f'Logistic Regression Recall: {recall_logistic}')
+
+'''
+From model_comparison we compared XGboosting, a Random Forest Classifier and a Logistic Regression Classifier.
+From the XGboost we got a recall=0.47, from RFC we got recall=0.46 and from logistic we got a recall=0.80
+So it appears that logistic regression is our best bet.
+'''
+def hyperparamter_tunning(path_file,model_name=LogisticRegression(class_weight='balanced')):
+    df = data.load_data(path_file)
+    df = data.clean_basic_issues(df)
+    df_encoded = data.deeper_clean(df, OneHotEncoding=True)
+    # Now we can split the data into training data and test data
+    X = df_encoded.drop("Churn_Yes", axis=1)
+    y = df_encoded["Churn_Yes"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    model = model_name
+    param_distributions = {
+        'C': loguniform(0.001, 100.0),  # Use a log-uniform distribution for C
+        'penalty': ['l1', 'l2', 'elasticnet', 'none'],
+        'solver': ['liblinear', 'saga'],  # 'liblinear' and 'saga' support both l1 and l2
+        'max_iter': [100, 500, 1000, 5000]  # A list of potential max_iter values
+    }
+    random_search = RandomizedSearchCV(
+        estimator=model,
+        param_distributions=param_distributions,
+        scoring='recall',
+        n_iter=50,  # Number of parameter settings that are sampled
+        cv=5,  # Number of cross-validation folds
+        verbose=1,
+        random_state=42,
+        n_jobs=-1  # Use all available cores
+    )
+    search_random =random_search.fit(X_train, y_train)
+    param_grid = [
+        {
+            'penalty': ['l1', 'l2'],
+            'solver': ['liblinear', 'saga'],
+            'C': [0.001, 0.01, 0.1, 1, 10, 100]
+        },
+        {
+            'penalty': ['l2', 'none'],  # 'none' penalty requires no C or l1_ratio
+            'solver': ['lbfgs', 'newton-cg', 'sag', 'saga'],
+            'C': [0.001, 0.01, 0.1, 1, 10, 100]
+        },
+        {
+            'penalty': ['elasticnet'],
+            'solver': ['saga'],
+            'l1_ratio': [0.1, 0.5, 0.9],  # l1_ratio is specific to elasticnet
+            'C': [0.001, 0.01, 0.1, 1, 10, 100]
+        }
+    ]
+    grid_search = GridSearchCV(
+        estimator=model,
+        param_grid=param_grid,
+        scoring='recall',
+        n_jobs=-1,
+        cv=5
+    )
+
+    search_grid = grid_search.fit(X_train, y_train)
+    print(f'The best recall score from Randomized is: {search_random.best_score_}')
+    print(f'The best recall score from Grid is {search_grid.best_score_}')
+
+
+
+
 
 if __name__ == "__main__":
     path_file = r"D:\Documentos\Vida_profesional\Coding\Projects\customer_churn_ml\data\raw\raw.csv"
-    print(basic_logistic_regression(path_file=path_file))
+    #print(basic_logistic_regression(path_file=path_file))
+    #model_comparison(path_file)
+    hyperparamter_tunning(path_file)
